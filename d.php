68b2473d39a2de00ae6f4d764e48e9b3a6578cfd
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-06-03 07:57:22 +0200

    Update d.php

--- d.php
+++ d.php
@@ -22,7 +22,7 @@
             echo"Podaj numer.";
         }else{
             $conn= new mysqli("localhost", "root", "", "songs") or die("Error");
-            $odp = $conn->query("SELECT * FROM queen union SELECT * FROM depechemode union SELECT * FROM pinkfloyd union SELECT * FROM rem union SELECT * FROM gunsnroses union SELECT * FROM nirvana");
+            $odp = $conn->query("SELECT * FROM queen union SELECT * FROM depechemode union SELECT * FROM pinkfloyd union SELECT * FROM rem union SELECT * FROM metallica union SELECT * FROM foofighters union SELECT * FROM gunsnroses union SELECT * FROM nirvana");
             $i=1;
             while($odp2 = $odp->fetch_array()){
                 if($i==$n){
@@ -31,11 +31,13 @@
                     echo"<th>ID</th>";
                     echo"<th>TYTUŁ</th>";
                     echo"<th>WYKONAWCA</th>";
+                    echo"<th>OCENA</th>";
                     echo"</tr>";
                     echo "<tr>";
                     echo "<td><center>". $i ."</center></td>";
                     echo "<td><center>". $odp2["title"]."</center></td>";
                     echo "<td><center>". $odp2["band"]."</center></td>";
+                    echo "<td><center>". $odp2["note"]."</center></td>";
                     echo "</tr>";
                     echo "</table></center>";
                 }
