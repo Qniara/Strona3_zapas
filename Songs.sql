a3b14f1d93bc21464ba6413c974f7b7d0e4983fb
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-06-03 07:56:10 +0200

    Update Songs.sql

--- Songs.sql
+++ Songs.sql
@@ -5,69 +5,91 @@ USE SONGS;
 CREATE TABLE Queen (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
-    band VARCHAR(255)
+    band VARCHAR(255),
+    note VARCHAR(255)
 );
 CREATE TABLE DepecheMode (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
-    band VARCHAR(255)
+    band VARCHAR(255),
+    note VARCHAR(255)
 );
 CREATE TABLE PinkFloyd (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
-    band VARCHAR(255)
+    band VARCHAR(255),
+    note VARCHAR(255)
 );
 CREATE TABLE REM (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
-    band VARCHAR(255)
+    band VARCHAR(255),
+    note VARCHAR(255)
+);
+CREATE TABLE Metallica (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    band VARCHAR(255),
+    note VARCHAR(255)
+);
+CREATE TABLE FooFighters (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    band VARCHAR(255),
+    note VARCHAR(255)
 );
 CREATE TABLE GunsNRoses (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
-    band VARCHAR(255)
+    band VARCHAR(255),
+    note VARCHAR(255)
 );
 CREATE TABLE Nirvana (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
-    band VARCHAR(255)
+    band VARCHAR(255),
+    note VARCHAR(255)
 );
 
 USE SONGS;
 
-INSERT INTO Queen (title, band) VALUES ('Is This The World We Created?', 'Queen');
-INSERT INTO Queen (title, band) VALUES ('Love Of My Life', 'Queen');
-INSERT INTO Queen (title, band) VALUES ('I Want To Break Free', 'Queen');
-INSERT INTO Queen (title, band) VALUES ('Fat Bottomed Girls', 'Queen');
-INSERT INTO Queen (title, band) VALUES ('Somebody To Love', 'Queen');
-INSERT INTO Queen (title, band) VALUES ('Crazy Little Thing Called Love', 'Queen');
-INSERT INTO Queen (title, band) VALUES ('39', 'Queen');
-INSERT INTO DepecheMode (title, band) VALUES ('Enjoy The Silence', 'Depeche Mode');
-INSERT INTO DepecheMode (title, band) VALUES ('Personal Jesus', 'Depeche Mode');
-INSERT INTO PinkFloyd (title, band) VALUES ('Money', 'Pink Floyd');
-INSERT INTO PinkFloyd (title, band) VALUES ('Another Brick In The Wall', 'Pink Floyd');
-INSERT INTO PinkFloyd (title, band) VALUES ('Wish You Were Here', 'Pink Floyd');
-INSERT INTO REM (title, band) VALUES ('Losing My Religion', 'R.E.M.');
-INSERT INTO REM (title, band) VALUES ('Man On Thre Moon', 'R.E.M.');
-INSERT INTO GunsNRoses (title, band) VALUES ('Sweet Child O Mine', 'Guns N Roses');
-INSERT INTO GunsNRoses (title, band) VALUES ('November Rain', 'Guns N Roses');
-INSERT INTO GunsNRoses (title, band) VALUES ('Dont Cry', 'Guns N Roses');
-INSERT INTO Nirvana (title, band) VALUES ('Rape Me', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('About A Girl', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Pennyroyal Tea', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Come As You Are', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Polly', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Heart-Shaped Box', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Where Did You Sleep Last Night', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Smells Like Teen Spirit', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Jesus Doesnt Want Me For A Sunbeam', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('And I Love Her', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Lithium', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('The Man Who Sold The World', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('All Apologies', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Lake Of Fire', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Dumb', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Oh, Me', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Frances Farmer Will Have Her Revenge On Seattle', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('Something In The Way', 'Nirvana');
-INSERT INTO Nirvana (title, band) VALUES ('On A Plain', 'Nirvana');
+INSERT INTO Queen (title, band, note) VALUES ('Is This The World We Created?', 'Queen', '5');
+INSERT INTO Queen (title, band, note) VALUES ('Love Of My Life', 'Queen', '5');
+INSERT INTO Queen (title, band, note) VALUES ('I Want To Break Free', 'Queen', '5');
+INSERT INTO Queen (title, band, note) VALUES ('Fat Bottomed Girls', 'Queen', '4');
+INSERT INTO Queen (title, band, note) VALUES ('Somebody To Love', 'Queen', '5');
+INSERT INTO Queen (title, band, note) VALUES ('Crazy Little Thing Called Love', 'Queen', '5');
+INSERT INTO Queen (title, band, note) VALUES ('39', 'Queen', '4');
+INSERT INTO Queen (title, band, note) VALUES ('Dont Stop Me Now', 'Queen', '5');
+INSERT INTO DepecheMode (title, band, note) VALUES ('Enjoy The Silence', 'Depeche Mode', '5');
+INSERT INTO DepecheMode (title, band, note) VALUES ('Personal Jesus', 'Depeche Mode', '4');
+INSERT INTO PinkFloyd (title, band, note) VALUES ('Money', 'Pink Floyd', '5');
+INSERT INTO PinkFloyd (title, band, note) VALUES ('Another Brick In The Wall', 'Pink Floyd', '5');
+INSERT INTO PinkFloyd (title, band, note) VALUES ('Wish You Were Here', 'Pink Floyd', '5');
+INSERT INTO REM (title, band, note) VALUES ('Losing My Religion', 'R.E.M.', '3');
+INSERT INTO REM (title, band, note) VALUES ('Man On Thre Moon', 'R.E.M.', '3');
+INSERT INTO GunsNRoses (title, band, note) VALUES ('Sweet Child O Mine', 'Guns N Roses', '5');
+INSERT INTO GunsNRoses (title, band, note) VALUES ('November Rain', 'Guns N Roses', '3,5');
+INSERT INTO GunsNRoses (title, band, note) VALUES ('Dont Cry', 'Guns N Roses', '4,5');
+INSERT INTO GunsNRoses (title, band, note) VALUES ('Patience', 'Guns N Roses', '5');
+INSERT INTO GunsNRoses (title, band, note) VALUES ('Yesterdays', 'Guns N Roses', '4');
+INSERT INTO Nirvana (title, band, note) VALUES ('Rape Me', 'Nirvana', '3,5');
+INSERT INTO Nirvana (title, band, note) VALUES ('About A Girl', 'Nirvana', '5');
+INSERT INTO Nirvana (title, band, note) VALUES ('Pennyroyal Tea', 'Nirvana', '4');
+INSERT INTO Nirvana (title, band, note) VALUES ('Come As You Are', 'Nirvana', '4');
+INSERT INTO Nirvana (title, band, note) VALUES ('Polly', 'Nirvana', '3,5');
+INSERT INTO Nirvana (title, band, note) VALUES ('Heart-Shaped Box', 'Nirvana', '5');
+INSERT INTO Nirvana (title, band, note) VALUES ('Where Did You Sleep Last Night', 'Nirvana', '4');
+INSERT INTO Nirvana (title, band, note) VALUES ('Smells Like Teen Spirit', 'Nirvana', '5');
+INSERT INTO Nirvana (title, band, note) VALUES ('Jesus Doesnt Want Me For A Sunbeam', 'Nirvana', '4');
+INSERT INTO Nirvana (title, band, note) VALUES ('And I Love Her', 'Nirvana', '5');
+INSERT INTO Nirvana (title, band, note) VALUES ('Lithium', 'Nirvana', '5');
+INSERT INTO Nirvana (title, band, note) VALUES ('The Man Who Sold The World', 'Nirvana', '5');
+INSERT INTO Nirvana (title, band, note) VALUES ('All Apologies', 'Nirvana', '3');
+INSERT INTO Nirvana (title, band, note) VALUES ('Lake Of Fire', 'Nirvana', '3');
+INSERT INTO Nirvana (title, band, note) VALUES ('Dumb', 'Nirvana', '5');
+INSERT INTO Nirvana (title, band, note) VALUES ('Oh, Me', 'Nirvana', '3,5');
+INSERT INTO Nirvana (title, band, note) VALUES ('Frances Farmer Will Have Her Revenge On Seattle', 'Nirvana', '4');
+INSERT INTO Nirvana (title, band, note) VALUES ('Something In The Way', 'Nirvana', '3');
+INSERT INTO Nirvana (title, band, note) VALUES ('On A Plain', 'Nirvana', '4');
+INSERT INTO Nirvana (title, band, note) VALUES ('Drain You', 'Nirvana', '4');
