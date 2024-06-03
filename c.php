a3098f283b9e5c24b623456b6f3f2976beee9702
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-06-03 07:57:05 +0200

    Update c.php

--- c.php
+++ c.php
@@ -15,7 +15,7 @@
     <?php
         $n=rand(1,36);
         $conn= new mysqli("localhost", "root", "", "songs") or die("Błąd");
-        $odp = $conn->query("SELECT * FROM queen union SELECT * FROM depechemode union SELECT * FROM pinkfloyd union SELECT * FROM rem union SELECT * FROM gunsnroses union SELECT * FROM nirvana");
+        $odp = $conn->query("SELECT * FROM queen union SELECT * FROM depechemode union SELECT * FROM pinkfloyd union SELECT * FROM rem union SELECT * FROM metallica union SELECT * FROM foofighters union SELECT * FROM gunsnroses union SELECT * FROM nirvana");
         $i=1;
         while($odp2 = $odp->fetch_array()){
             if($i==$n){
@@ -24,11 +24,13 @@
                 echo"<th>ID</th>";
                 echo"<th>TYTUŁ</th>";
                 echo"<th>WYKONAWCA</th>";
+                echo"<th>OCENA</th>";
                 echo"</tr>";
                 echo "<tr>";
                 echo "<td><center>". $i ."</center></td>";
                 echo "<td><center>". $odp2["title"]."</center></td>";
                 echo "<td><center>". $odp2["band"]."</center></td>";
+                echo "<td><center>". $odp2["note"]."</center></td>";
                 echo "</tr>";
                 echo "</table></center>";
             }
